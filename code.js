b8102913ca2127ad433311d0768dd7ab71e34a21
jonasbergmar <jonas.bergmar@hotmail.com>
2023-11-09 16:44:22 +0100

    pausar

--- code.js
+++ code.js
@@ -1,11 +1,8 @@
+// const backsideFile = "backside.png";
 import { cards } from "./cards.js";
 const gameEl = document.getElementById("game");
-
-// const backsideFile = "backside.png";
-
 // 1. Med forEach Lägg till en property showing: false till varje kort
 // för att hålla reda på om kortet är visas eller inte.
-
 // 2. Skapa en funktion createCard. Som tar ett card-objekt och ett index som input och skapar
 // ett img element och lägger till det på sidan i gameEl. Gör så här:
 // - skapa ett ett img-element med document.createElement("img")
@@ -19,6 +16,31 @@ const gameEl = document.getElementById("game");
 
 // 3. Använd for-loop eller forEach för att loopa igenom alla cards och anrop funktionen
 //    createCard med varje kort och varje index (i). Nu ska alla kort synas på sidan
+cards.forEach((card) => {
+  card.showing = false;
+});
+
+function createCard(card, index) {
+  const imgElement = document.createElement("img");
+  imgElement.setAttribute("src", "images/backside.png");
+  imgElement.id = index;
+  imgElement.style.width = "100px";
+  imgElement.style.height = "145px";
+
+  imgElement.addEventListener("click", function () {
+    if (card.showing === true) {
+      imgElement.setAttribute("src", card.file);
+      card.showing = false;
+    } else {
+      imgElement.setAttribute("src", "images/backside.png");
+      card.showing = true;
+    }
+  });
+  gameEl.appendChild(imgElement);
+}
+cards.forEach((card, index) => {
+  createCard(card, index);
+});
 
 // 4. Lägg till addEventListner på korten i funktionen createCard. När man klickar ska
 // kortet ändras från showing: false till showing:true
@@ -27,3 +49,5 @@ const gameEl = document.getElementById("game");
 // kolla när man klickar på ett kort om det har samma värde som lastCard (card.num)
 
 // 6 (Ganska svårt) FÅ ihop hela spelet med det du skrivit ovan.
+
+//Gör en funktion för att visa kortens baksida när resultat är false.
